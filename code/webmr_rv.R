feed24012503f78f26831c3646c932d36ae981ca
Stanley <Ryan.Stanley@dfo-mpo.gc.ca>
2026-01-26 15:22:28 -0400

    Figure 3 french translation

--- code/webmr_rv.R
+++ code/webmr_rv.R
@@ -119,6 +119,8 @@ combo_plot <- p1 + inset_element(p2, left = 0.55, bottom = 0.05, right = 1, top 
 
 ggsave("output/webca_rv_combo.png",combo_plot,height=5,width=5,units="in",dpi=600)
 
+
+
 ##make a plot with the RV strata with full extent
 
 webmr <- maritimes_network%>%filter(name=="Western/Emerald Banks Marine Refuge")
@@ -133,14 +135,16 @@ rv_area_df <- rv_strata%>%
 
 webmr_strata <- webmr_strata%>%
                 mutate(prop_area = round(webmr_strata$area/rv_area_df$area,3)*100,
-                       lab = paste0(StrataID," (",prop_area,"%)"))%>%
+                       lab = paste0(StrataID," (",prop_area,"%)"),
+                       lab_fr = gsub("(\\d)\\.(\\d%)", "\\1,\\2",lab))%>%
                 arrange(-prop_area)%>%
-                mutate(lab = factor(lab,levels=lab))%>%
+                mutate(lab = factor(lab,levels=lab),
+                       lab_fr=factor(lab_fr,levels=lab_fr))%>%
                 filter(as.numeric(prop_area)>5) #cut out the small areas
 
 webmr_strata_full <- rv_strata%>%
                      filter(StrataID %in% unique(webmr_strata$StrataID))%>%
-                     left_join(.,webmr_strata%>%data.frame()%>%dplyr::select(StrataID,lab))
+                     left_join(.,webmr_strata%>%data.frame()%>%dplyr::select(StrataID,lab,lab_fr))
 
 plot_lims2 <- rv_strata%>%
               filter(StrataID %in% unique(webmr_strata$StrataID))%>%
@@ -203,3 +207,80 @@ ggsave(combo_path,combo_plot2,height=5,width=9,units="in",dpi=300)
 image <- image_read(combo_path)
 
 image_write(image_trim(image), path = combo_path)
+
+
+
+#### Translated 
+
+#Translated version 
+
+rv_df <- rv_df%>%
+  mutate(set_type_fr = ifelse(set_type == "Primary set","Trait principal","Trait secondaire"))
+
+p5 <- ggplot()+
+  geom_sf(data=basemap_atlantic)+
+  geom_sf(data=maritimes_network%>%filter(name!="Western/Emerald Banks Marine Refuge"),fill="grey90",alpha=0.5)+
+  geom_sf(data=rv_strata,fill=NA,col="grey80",lwd=0.3)+
+  geom_sf(data=webmr_strata_full,aes(fill=lab_fr),alpha=0.7)+
+  geom_sf(data=webmr_strata,aes(fill=lab_fr))+
+  geom_sf(data=webmr,fill=NA,lwd=1.2)+
+  geom_sf(data=rv_df%>%filter(TYPE=="PRIMARY"),aes(shape=set_type),fill="cornflowerblue",col="black",size=2.5,show.legend = FALSE)+
+  geom_sf(data=rv_df%>%filter(TYPE!="PRIMARY"),aes(shape=set_type),col="black",size=1,show.legend = FALSE)+
+  scale_shape_manual(values=c(21,3))+
+  scale_x_continuous(
+    labels = function(x) paste0(abs(x), "° O")
+  )+
+  scale_y_continuous(
+    labels = function(y) paste0(abs(y), "° N")
+  )+
+  
+  theme_bw()+
+  theme(axis.title = element_blank(),
+        #legend.position = "none",
+        axis.text = element_blank(),
+        axis.ticks = element_blank(),
+        plot.margin = unit(c(0, 0, 0, 0), "lines"))+
+  coord_sf(expand=0,xlim=plot_lims2[c(1,3)],ylim=plot_lims2[c(2,4)])+
+  labs(fill="Strate de relevé")+
+  annotation_scale(location = "br")
+
+p6 <- ggplot()+
+  geom_sf(data=rv_strata,fill=NA,col="grey80",lwd=0.3)+
+  geom_sf(data=bioregion,fill=NA)+
+  geom_sf(data=basemap_atlantic)+
+  geom_sf(data=maritimes_network%>%filter(name!="Western/Emerald Banks Marine Refuge"),fill="grey90",alpha=0.5)+
+  geom_sf(data=maritimes_network%>%filter(name=="Western/Emerald Banks Marine Refuge"),linewidth=1.2,fill="grey90",alpha=0.5,col="black")+
+  geom_sf(data=rv_df%>%filter(TYPE=="PRIMARY"),aes(shape=set_type_fr),fill="cornflowerblue",col="black",size=1)+
+  geom_sf(data=rv_df%>%filter(TYPE!="PRIMARY"),aes(shape=set_type_fr),col="black",size=0.6)+
+  scale_shape_manual(values=c(21,3))+
+  
+  scale_x_continuous(
+    labels = function(x) paste0(abs(x), "° O")
+  )+
+  scale_y_continuous(
+    labels = function(y) paste0(abs(y), "° N")
+  )+
+  
+  geom_sf(data=webca_box_poly,lty=2,lwd=0.5,fill=NA)+    
+  theme_bw()+
+  theme(axis.title = element_blank(),
+        legend.position="inside",
+        legend.position.inside = c(0.84,0.92),
+        legend.title = element_blank(),
+        legend.background = element_blank())+
+  coord_sf(xlim=plotlims[c(1,3)],ylim=plotlims[c(2,4)])+
+  annotation_scale()
+
+#assemble and save
+combo_plot3 <- p6 + p5 + plot_layout(ncol=2)
+
+
+combo_path_2 <- "output/rv_strata_webmr_plot_fr.png"
+
+ggsave(combo_path_2,combo_plot3,height=5,width=9,units="in",dpi=300)
+
+image <- image_read(combo_path_2)
+
+image_write(image_trim(image), path = combo_path_2)
+
+
