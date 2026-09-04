928463f09c3a99b560312fbd1758a9bf3a5ef3f3
Stanley <Ryan.Stanley@dfo-mpo.gc.ca>
2026-09-04 14:51:19 -0300

    translation
    translation for CSAS alignment with figure order in document

--- code/webmr_rv.R
+++ code/webmr_rv.R
@@ -10,7 +10,6 @@ library(ggspatial)
 library(terra)
 library(tidyterra)
 library(rasterVis)
-library(basemaps)
 library(viridis)
 library(scales)
 library(patchwork)
@@ -30,7 +29,8 @@ bioregion <- data_planning_areas()%>%
 rv_df <- read.csv("c:/Users/stanleyr/Documents/Github/mar_network/data/eDNA sampling/2024_Summer_Stations.csv")%>%
          st_as_sf(coords=c("LON_DD","LAT_DD"),crs=latlong)%>%
          st_transform(CanProj)%>%
-         mutate(set_type = ifelse(TYPE=="PRIMARY","Primary set","Secondary set"))
+         mutate(set_type = ifelse(TYPE=="PRIMARY","Primary set","Secondary set"),
+                set_type_fr = ifelse(TYPE=="PRIMARY","Trait primaire","Trait secondaire"))
 
 rv_strata <- read_sf("c:/Users/stanleyr/Documents/Github/mar_network/data/shapefiles/MaritimesRegionEcosystemAssessmentStrata(2014-).shp")%>%
              st_transform(CanProj)%>%
@@ -100,6 +100,25 @@ p1 <- ggplot()+
   coord_sf(xlim=plotlims[c(1,3)],ylim=plotlims[c(2,4)])+
   annotation_scale()
 
+p1_fr <- ggplot()+
+  geom_sf(data=rv_strata,fill=NA,col="grey80",lwd=0.3)+
+  geom_sf(data=bioregion,fill=NA)+
+  geom_sf(data=basemap_atlantic)+
+  geom_sf(data=maritimes_network%>%filter(name!="Western/Emerald Banks Marine Refuge"),fill="grey90",alpha=0.5)+
+  geom_sf(data=maritimes_network%>%filter(name=="Western/Emerald Banks Marine Refuge"),linewidth=1.2,fill="grey90",alpha=0.5,col="black")+
+  geom_sf(data=rv_df%>%filter(TYPE=="PRIMARY"),aes(shape=set_type_fr),fill="cornflowerblue",col="black",size=1)+
+  geom_sf(data=rv_df%>%filter(TYPE!="PRIMARY"),aes(shape=set_type_fr),col="black",size=0.6)+
+  scale_shape_manual(values=c(21,3))+
+  geom_sf(data=webca_box_poly,lty=2,lwd=0.5,fill=NA)+    
+  theme_bw()+
+  theme(axis.title = element_blank(),
+        legend.position="inside",
+        legend.position.inside = c(0.84,0.92),
+        legend.title = element_blank(),
+        legend.background = element_blank())+
+  coord_sf(xlim=plotlims[c(1,3)],ylim=plotlims[c(2,4)])+
+  annotation_scale()
+
 p2 <- ggplot()+
   geom_sf(data=rv_strata,fill=NA,col="grey80",lwd=0.3)+
   geom_sf(data=maritimes_network%>%filter(name!="Western/Emerald Banks Marine Refuge"),fill="grey90",alpha=0.5)+
@@ -115,10 +134,13 @@ p2 <- ggplot()+
         plot.margin = unit(c(0, 0, 0, 0), "lines"))+
   coord_sf(xlim=webca_box[c(1,3)],ylim=webca_box[c(2,4)])
 
+#combo plots
 combo_plot <- p1 + inset_element(p2, left = 0.55, bottom = 0.05, right = 1, top = 0.5)
+combo_plot_fr <- p1_fr + inset_element(p2, left = 0.55, bottom = 0.05, right = 1, top = 0.5)
 
-ggsave("output/webca_rv_combo.png",combo_plot,height=5,width=5,units="in",dpi=600)
-
+#save plots
+ggsave("output/Fig20.png",combo_plot,height=5,width=5,units="in",dpi=600)
+ggsave("output/Fig20_fr.png",combo_plot_fr,height=5,width=5,units="in",dpi=600)
 
 
 ##make a plot with the RV strata with full extent
